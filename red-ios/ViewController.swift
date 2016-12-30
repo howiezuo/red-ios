5793321271c2a66c3d7c93c93613940e748c885f
Howie Zuo <howie@Howies-MacBook-Pro.local>
2016-12-30 23:34:37 +0900

    new project

---
+++ red-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  red-ios
+//
+//  Created by Howie Zuo on 12/30/16.
+//  Copyright © 2016 Howie Zuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
